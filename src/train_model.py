8ac181f0026925dd90971d600aaa5eb782047f46
niharikap749 <niharikap749@gmail.com>
2025-12-18 16:00:41 +0530

    Add Random Forest model and compare with logistic regression

--- src/train_model.py
+++ src/train_model.py
@@ -1,6 +1,7 @@
 from preprocess import preprocess_data
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn.ensemble import RandomForestClassifier
 
 X_train, X_test, y_train, y_test = preprocess_data("../data/loan_data.csv")
 model = LogisticRegression(max_iter=1000)
@@ -8,4 +9,18 @@ model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 print("Accuracy:", accuracy_score(y_test, y_pred))
 print("\nClassification Report:\n", classification_report(y_test, y_pred))
+
+# ---- Random Forest Model ----
+rf_model = RandomForestClassifier(
+    n_estimators=100,
+    random_state=42
+)
+
+rf_model.fit(X_train, y_train)
+
+rf_pred = rf_model.predict(X_test)
+
+print("\nRandom Forest Results")
+print("Accuracy:", accuracy_score(y_test, rf_pred))
+print("\nClassification Report:\n", classification_report(y_test, rf_pred))
 
